cd1320221e825437b58e4f04d52f4fcb44a6f67f
eduardogutier43 <33366100-eduardogutier43@users.noreply.replit.com>
2024-04-15 22:55:44 +0000

    Commit de prueba

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import { handleOperations } from './routes';
+import express from "express";
+import { handleOperations } from "./routes";
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.post('/operate', handleOperations);
+app.post("/operate", handleOperations);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
